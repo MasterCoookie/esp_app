827302a57629a99242ca50612870fd8f194ed218
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-25 12:14:24 +0200

    Device widget reads args

--- lib/screens/home/deviceWidget.dart
+++ lib/screens/home/deviceWidget.dart
@@ -2,19 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:esp_app/services/device.dart';
 
 class DeviceWidget extends StatefulWidget {
-  final Device device;
-
-  const DeviceWidget({Key key, this.device}) : super(key: key);
-
-  
-
   @override
   State<DeviceWidget> createState() => _DeviceWidgetState();
 }
 
 class _DeviceWidgetState extends State<DeviceWidget> {
+  
+
   @override
   Widget build(BuildContext context) {
-    return Text(widget.device.deviceName != null ? widget.device.deviceName : 'Unnamed Device');
+    final args = ModalRoute.of(context).settings.arguments as Device;
+    return Text(args.deviceName);
   }
 }
