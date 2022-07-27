3df25a1d7fe97d82c9fba4d99b8027bc225bdbb3
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-27 23:42:55 +0200

    adds successfull dupa test

--- lib/services/bt.dart
+++ lib/services/bt.dart
@@ -1,20 +1,34 @@
 import 'package:quick_blue/quick_blue.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:typed_data';
 
 class BT {
   String name;
 
   BT(name) {
     QuickBlue.setConnectionHandler(_handleConnectionChange);
+    QuickBlue.setServiceHandler(_handleServiceDiscovery);
     this.name = name;
   }
 
   void _handleConnectionChange(String deviceId, BlueConnectionState state) {
     print('_handleConnectionChange $deviceId, $state');
+    if(state.value == "connected") {
+      QuickBlue.discoverServices(deviceId);
+    }
   }
 
   void _handleServiceDiscovery(String deviceId, String serviceId) {
-    print('_handleServiceDiscovery $deviceId, $serviceId');
+    print('_handleServiceDiscovery $deviceId, $serviceId');    
+    if(serviceId == "4fafc201-1fb5-459e-8fcc-c5c9c331914b") {
+      print("4fafc201-1fb5-459e-8fcc-c5c9c331914b service found!");
+      List<int> list = 'dupa'.codeUnits;
+      Uint8List bytes = Uint8List.fromList(list);
+    
+
+      QuickBlue.writeValue(deviceId, "4fafc201-1fb5-459e-8fcc-c5c9c331914b", "beb5483e-36e1-4688-b7f5-ea07361b26a8", bytes, BleOutputProperty.withResponse);
+      QuickBlue.stopScan();
+    }
   }
 
   Future scanAndConnect(String MAC) async {
@@ -24,8 +38,10 @@ class BT {
       if(result.deviceId == MAC) {
         print("matching device found! Connecting...");
         QuickBlue.connect(result.deviceId);
-        QuickBlue.discoverServices(result.deviceId);
-        QuickBlue.stopScan();
+        
+        
+
+        
       }
     });
 
