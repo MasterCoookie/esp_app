e117107a3b921405a8de80d2ff368ac27813b5d6
MasterCoookie <jas.kocurek2@wp.pl>
2022-07-31 13:28:31 +0200

    DeviceWidgetSettings form

--- lib/screens/home/deviceSettings.dart
+++ lib/screens/home/deviceSettings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:esp_app/services/bt.dart';
+import 'package:esp_app/constants.dart';
 
 class DeviceWidgetSettings extends StatefulWidget {
   final BT bt;
@@ -11,17 +12,36 @@ class DeviceWidgetSettings extends StatefulWidget {
 
 class _DeviceWidgetSettingsState extends State<DeviceWidgetSettings> {
   final _formKey = GlobalKey<FormState>();
-
+  int value;
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Form(
-        key: _formKey,
-        child: Column(children: [
-          
-        ],)
-      )
+    return SingleChildScrollView(
+      child: Container(
+        color: Color(colorPalette["bcg_secondary"]),
+        child: Form(
+          key: _formKey,
+          child: Column(children: <Widget>[
+            SizedBox(height: 18),
+            Text("Device Settings", style: TextStyle(fontSize: 18)),
+            SizedBox(height: 18),
+            SizedBox(
+              width: 200,
+              child: TextFormField(
+                decoration: textInputFieldDecoration.copyWith(hintText: "Motor Speeed (rpm)"),
+                onChanged: (val) { this.value = int.parse(val); }
+              ),
+            ), SizedBox(height: 12),
+            ElevatedButton(
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Color(colorPalette["alt_strong"]))),
+              child: Text("Save", style: TextStyle(color: Colors.white)),
+              onPressed: () {
+
+              }
+            )
+          ],)
+        )
+      ),
     );
   }
 }
