9fc8fc41d5e2e931c9d4a579a82d6fc87ff0eb13
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-03 19:52:51 +0200

    deviceConfig added to routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:esp_app/screens/wrapper.dart';
 import 'package:esp_app/screens/home/deviceWidget.dart';
+import 'package:esp_app/screens/home/deviceConfig.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: { '/deviceWidget': (context) => DeviceWidget() },
+      routes: {
+        '/deviceWidget': (context) => DeviceWidget(),
+        '/deviceConfig': (context) => DeviceConfig()
+        },
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.red,
