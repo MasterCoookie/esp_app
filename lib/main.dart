eb7f5dc56055045c733cd5258df85a2bce436c78
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-11 18:23:07 +0200

    https ignores bad handshakes

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:esp_app/screens/wrapper.dart';
 import 'package:esp_app/screens/home/deviceWidget.dart';
 import 'package:esp_app/screens/home/deviceConfig.dart';
+import 'package:esp_app/services/httpoerrides.dart';
+import 'dart:io';
 
 void main() {
+  HttpOverrides.global = new DevHttpOverrides();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
