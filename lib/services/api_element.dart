d5fca46fec18a3dad4594bd3eeeaf1396f3025a5
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-11 17:46:42 +0200

    api_address changed to https

--- lib/services/api_element.dart
+++ lib/services/api_element.dart
@@ -4,7 +4,7 @@ import 'package:esp_app/services/user.dart';
 class ApiElement {
   String _id;
   static const headers = { 'Content-Type': 'application/json;charset=UTF-8' };
-  static const api_address = "http://192.168.0.174:8080/";
+  static const api_address = "https://192.168.0.174:8080/";
   // static const api_address = "http://10.0.2.2:8080/";
   static final encoding = Encoding.getByName('utf-8');
 
