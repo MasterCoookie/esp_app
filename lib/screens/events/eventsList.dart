26a253d111df18a23b0a4263948db350a4dd6987
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-23 17:24:02 +0200

    eventsList basic body

--- lib/screens/events/eventsList.dart
+++ lib/screens/events/eventsList.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:esp_app/services/device.dart';
 
-class EventsList extends StatelessWidget {
-  const EventsList({Key key}) : super(key: key);
+class EventsList extends StatefulWidget {
+  @override
+  State<EventsList> createState() => _EventsListState();
+}
 
+class _EventsListState extends State<EventsList> {
+  
   @override
   Widget build(BuildContext context) {
-    
+    final args = ModalRoute.of(context).settings.arguments as Device;
+    Scaffold(
+      appBar: AppBar(
+        title: Text(args.deviceName)
+      ),
+    );
   }
 }
