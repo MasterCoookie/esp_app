da408169ac069509fedb42e9bbb5a800a86cc6a6
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-24 12:45:24 +0200

    repeat days showing work in progress

--- lib/screens/events/eventRepeatitions.dart
+++ lib/screens/events/eventRepeatitions.dart
@@ -4,10 +4,29 @@ import 'package:esp_app/constants.dart';
 
 class EventRepeatitions extends StatelessWidget {
   final DeviceEvent event;
+  final weekdays = ["SU", "MO", "TU", "WE", "TH", "FR", "ST"];
   EventRepeatitions({ this.event });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: SizedBox(
+        height: 20,
+        child: ListView.builder(shrinkWrap: true, scrollDirection: Axis.horizontal, itemCount: event.repeat.length, itemBuilder: (context, index) {
+          TextStyle style;
+          if(event.repeat[index]) {
+            style = TextStyle();
+          } else {
+            style = TextStyle();
+          }
+          
+          return Padding(
+            padding: const EdgeInsets.all(1.5),
+            child: Text(weekdays[index], style: style),
+          );
+        }),
+      )
+  
+      );
   }
 }
