2fdba116e7d8b31f096fcf021c5d6adaaf264c85
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-25 13:28:55 +0200

    EventEditor widget and time picker

--- lib/screens/events/eventEditor.dart
+++ lib/screens/events/eventEditor.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:esp_app/services/device.dart';
 import 'package:esp_app/services/deviceEvent.dart';
+import 'package:esp_app/constants.dart';
 
 class EventEditor extends StatefulWidget {
 
@@ -11,6 +12,27 @@ class EventEditor extends StatefulWidget {
 class _EventEditorState extends State<EventEditor> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final args = ModalRoute.of(context).settings.arguments as Device;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.deviceName + " event")
+      ),
+      backgroundColor: Color(colorPalette["secondary"]),
+      body: Container(
+        child: Column(children: [
+          SizedBox(height: 24),
+          ListTile(
+            title: Center(child: Text("12:00", style: TextStyle(color: Colors.white, fontSize: 38))),
+            onTap: () async {
+              final TimeOfDay newTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay(hour: 12, minute: 0),
+                initialEntryMode: TimePickerEntryMode.dial,
+              );
+            },
+          )
+        ])
+      )
+    );
   }
 }
