dbc737def0bf24e2455a2ccfb170c9435b98e392
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-25 13:09:21 +0200

    EventEditor widget created

---
+++ lib/screens/events/eventEditor.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:esp_app/services/device.dart';
+import 'package:esp_app/services/deviceEvent.dart';
+
+class EventEditor extends StatefulWidget {
+
+  @override
+  State<EventEditor> createState() => _EventEditorState();
+}
+
+class _EventEditorState extends State<EventEditor> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
