437c9c495afff7c08dbf4945e23792090418c7ec
MasterCoookie <jas.kocurek2@wp.pl>
2022-08-29 22:33:20 +0200

    EventEditorArgs class created

--- lib/services/args.dart
+++ lib/services/args.dart
@@ -1,9 +1,17 @@
 import 'package:esp_app/services/device.dart';
 import 'package:esp_app/services/bt.dart';
+import 'package:esp_app/services/deviceEvent.dart';
 
 class ConfigArgs {
   final Device device;
   final BT bt;
 
   ConfigArgs(this.device, this.bt);
+}
+
+class EventEditorArgs {
+  final Device device;
+  final DeviceEvent event;
+
+    EventEditorArgs(this.device, this.event);
 }
