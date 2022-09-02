ef83c368c69aa80c5c133f4e6f13dbc99e1b8ead
MasterCoookie <jas.kocurek2@wp.pl>
2022-09-02 14:02:20 +0200

    centering deviceWidget controlls

--- lib/screens/home/deviceWidget.dart
+++ lib/screens/home/deviceWidget.dart
@@ -41,50 +41,52 @@ class _DeviceWidgetState extends State<DeviceWidget> {
               Navigator.pushNamed(context, '/deviceConfig', arguments: ConfigArgs(args, bluetoothLE));
             })],
       ),
-      body: Column(
-        children: [
-        SizedBox(height: 20),
-        FutureBuilder(
-          future: bluetoothLE.scanAndConnect(),
-          builder: (context, snapshot) {
-            if(snapshot.hasData) {
-              bluetoothLE.stopScan();
-            
-            return Column(
-              children: [
-                IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.keyboard_double_arrow_up), onPressed: () { widget.curtain.curtainClose(bluetoothLE, CharacteristicType.remote); }),
-                GestureDetector(
-                  child: IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.arrow_upward_sharp), onPressed: () {}),
-                  onLongPressDown: (details) async {
-                    widget.curtain.curtainMove(false, bluetoothLE, CharacteristicType.remote);
-                  },
-                  onLongPressUp: () {
-                    widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
-                  },
-                  onLongPressCancel: () {
-                    widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
-                  }),
-                GestureDetector(
-                  child: IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.arrow_downward_sharp), onPressed: () {}),
-                  onLongPressDown: (details) {
-                    widget.curtain.curtainMove(true, bluetoothLE, CharacteristicType.remote);
-                  },
-                  onLongPressUp: () {
-                    widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
-                  },
-                  onLongPressCancel: () {
-                    widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
-                  }),
-                  IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.keyboard_double_arrow_down), onPressed: () { widget.curtain.curtainOpen(bluetoothLE, CharacteristicType.remote); }),
-              ],
-            );
-            } else {
-              return Text("Connecting");
+      body: Center(
+        child: Column(
+          children: [
+          SizedBox(height: 20),
+          FutureBuilder(
+            future: bluetoothLE.scanAndConnect(),
+            builder: (context, snapshot) {
+              if(snapshot.hasData) {
+                bluetoothLE.stopScan();
+              
+              return Column(
+                children: [
+                  IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.keyboard_double_arrow_up), onPressed: () { widget.curtain.curtainClose(bluetoothLE, CharacteristicType.remote); }),
+                  GestureDetector(
+                    child: IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.arrow_upward_sharp), onPressed: () {}),
+                    onLongPressDown: (details) async {
+                      widget.curtain.curtainMove(false, bluetoothLE, CharacteristicType.remote);
+                    },
+                    onLongPressUp: () {
+                      widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
+                    },
+                    onLongPressCancel: () {
+                      widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
+                    }),
+                  GestureDetector(
+                    child: IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.arrow_downward_sharp), onPressed: () {}),
+                    onLongPressDown: (details) {
+                      widget.curtain.curtainMove(true, bluetoothLE, CharacteristicType.remote);
+                    },
+                    onLongPressUp: () {
+                      widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
+                    },
+                    onLongPressCancel: () {
+                      widget.curtain.curtainStop(bluetoothLE, CharacteristicType.remote);
+                    }),
+                    IconButton(iconSize: 50, color: Colors.white, icon: Icon(Icons.keyboard_double_arrow_down), onPressed: () { widget.curtain.curtainOpen(bluetoothLE, CharacteristicType.remote); }),
+                ],
+              );
+              } else {
+                return Text("Connecting");
+              }
             }
-          }
+          ),
+            
+          ],
         ),
-          
-        ],
       ),
     );
   }
