2cc43c28a17712030395d8aed7f4827c878a2363
MasterCoookie <jas.kocurek2@wp.pl>
2022-09-13 12:34:49 +0200

    disconnecting bt on deviceWidget start

--- lib/screens/home/deviceWidget.dart
+++ lib/screens/home/deviceWidget.dart
@@ -22,6 +22,7 @@ class _DeviceWidgetState extends State<DeviceWidget> {
     final args = ModalRoute.of(context).settings.arguments as Device;
     BT bluetoothLE = new BT(args.deviceName, args.MAC);
     bluetoothLE.disconnect();
+    
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(
